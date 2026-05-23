88c9685d9785d79440f2d7a079fe80c1ad71522a
ty <zxcv6564@gmail.com>
2026-05-23 16:46:46 +0800

    Restore previous PWA install structure

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = "diary-integration-v9";
+const CACHE_NAME = "diary-integration-v10";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css",
   "./app.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
+  "./manifest.webmanifest",
+  "./icons/icon-192.svg",
+  "./icons/icon-512.svg",
 ];
 
 self.addEventListener("install", (event) => {
@@ -31,12 +31,13 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
         const copy = response.clone();
         caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
         return response;
-      })
-      .catch(() => caches.match(event.request))
+      });
+    })
   );
 });
