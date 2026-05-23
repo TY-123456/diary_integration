d6e3f3500aea0f7d8d0b9c6b44f9a30c101bfe72
ty <zxcv6564@gmail.com>
2026-05-23 16:20:06 +0800

    Restore SVG PWA install settings

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = "diary-integration-v5";
+const CACHE_NAME = "diary-integration-v6";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css",
   "./app.js",
   "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
+  "./icons/icon-192.svg",
+  "./icons/icon-512.svg",
 ];
 
 self.addEventListener("install", (event) => {
