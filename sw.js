c459d39718f68c7941f1d7359fddbcf9f93e2162
ty <zxcv6564@gmail.com>
2026-05-23 15:01:17 +0800

    Fix Android PWA install icons

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = "diary-integration-v3";
+const CACHE_NAME = "diary-integration-v4";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css",
   "./app.js",
   "./manifest.webmanifest",
-  "./icons/icon-192.svg",
-  "./icons/icon-512.svg",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
